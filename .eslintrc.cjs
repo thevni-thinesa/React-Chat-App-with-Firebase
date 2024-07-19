aeedc171798616114fdafae8feda017a0c772aa0
Thevni Thinesa <153171327+thevni-thinesa@users.noreply.github.com>
2024-07-19 09:57:49 +0530

    Update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,6 +7,7 @@ module.exports = {
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
   ],
+  
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
@@ -19,5 +20,6 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    
   },
 }
