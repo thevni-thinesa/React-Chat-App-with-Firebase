33de1b8022442f6f889748f0e7857bc1d1ba174b
Thevni <153171327+Thevni@users.noreply.github.com>
2024-07-22 17:00:27 +0530

    new updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,81 @@
+/*import Chat from "./components/chat/Chat";
+import Detail from "./components/detail/Detail";
+import List from "./components/list/List";
+import Login from "./components/login/Login";
+import Notification from "./components/notification/Notification";
+import { useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./lib/firebase";
+import { useUserStore } from "./lib/userStore";
+
+const App = () => {
+  //const user = false;
+  const {currentUser, isLoading, fetchUserInfo} = useUserStore();
+
+  useEffect(() => {
+    const unSub = onAuthStateChanged(auth, (user) => {
+      fetchUserInfo(user.uid);
+    });
+
+    return () => {
+      unSub();
+    };
+  }, [fetchUserInfo]);
+
+  console.log(currentUser)
+
+  if (isLoading) return <div className="loading">Loading...</div>;
+
+  return (
+    <div className='container'>
+      {currentUser ? (
+        <>
+          <List />
+          <Chat />
+          <Detail />
+        </>
+      ) : (
+        <Login/>
+      )}
+      <Notification />
+    </div>
+  );
+};
+export default App;*/
+
+
 import Chat from "./components/chat/Chat";
 import Detail from "./components/detail/Detail";
 import List from "./components/list/List";
 import Login from "./components/login/Login";
 import Notification from "./components/notification/Notification";
+import { useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./lib/firebase";
+import { useUserStore } from "./lib/userStore";
 
 const App = () => {
-  const user = false;
+  const {currentUser, isLoading, fetchUserInfo} = useUserStore();
+
+  useEffect(() => {
+    const unSub = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        fetchUserInfo(user.uid);
+      }
+    });
+
+    return () => {
+      unSub();
+    };
+  }, [fetchUserInfo]);
+
+  //console.log(currentUser);
+
+  if (isLoading) return <div className="loading">Loading...</div>;
 
   return (
     <div className='container'>
-      {user ? (
+      {currentUser ? (
         <>
           <List />
           <Chat />
--- src/components/list/userinfo/Userinfo.jsx
+++ src/components/list/userinfo/Userinfo.jsx
@@ -1,11 +1,14 @@
 import "./userinfo.css";
+import { useUserStore } from "../../../lib/userStore";
 
 const Userinfo = () => {
+
+  const {currentUser} = useUserStore();
     return (
       <div className='userinfo'>
         <div className="user">
-            <img src="./avatar.png" alt="" />
-            <h4>Thevni</h4>
+            <img src={currentUser.avatar || "./avatar.png"} alt="" />
+            <h4>{currentUser.username}</h4>
         </div>
         <div className="icons">
             <img src="./more.png" alt="" />
